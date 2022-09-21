d6a298a46d4561d4df67ebb3e266398a5781f4c2
Diana Stoyanova <diana.mstoya@gmail.com>
2022-09-21 21:34:36 +0200

    text operations

---
+++ countries_challenge.py
@@ -0,0 +1,28 @@
+input_filename = 'country_info.txt'
+
+countries = {}
+with open(input_filename) as country_file:
+    country_file.readline()
+    for row in country_file:
+        data = row.strip('\n').split('|')
+        country, capital, code, code3, dialing, timezone, currency = data
+        # print(country, capital, code, code3, dialing, timezone, currency, sep='\n\t')
+        country_dict = {
+            'name': country,
+            'capital': capital,
+            'country_code': code,
+            'cc3': code3,
+            'dialing_code': dialing,
+            'timezone': timezone,
+            'currency': currency,
+        }
+        # print(country_dict)
+        countries[country.casefold()] = country_dict
+        # code_lookup[code.casefold()] = country
+        countries[code.casefold()] = country_dict
+
+# print(countries)
+for key in countries:
+    for secondary_key in countries[key]:
+        if countries[key][secondary_key] == '':
+            print(key, secondary_key)
--- stripping.py
+++ stripping.py
@@ -1,3 +1,17 @@
+def remove_prefix(string: str, prefix: str) -> str:
+    if string.startswith(prefix):
+        return string[len(prefix):]
+    else:
+        return string[:]
+
+
+def remove_suffix(string: str, suffix: str) -> str:
+    if string.endswith(suffix):
+        return string[:-len(suffix)]
+    else:
+        return string[:]
+
+
 filename = 'Jabberwocky.txt'
 with open(filename) as poem:
     first = poem.readline().rstrip()
@@ -20,3 +34,13 @@ for character in first[::-1]:
         print(f'removing "{character}"')
     else:
         break
+
+print('*'*80)
+
+twas_removed = first.removeprefix("'Twas")
+print(twas_removed)
+toves_removed = first.removesuffix("toves")
+print(toves_removed)
+print('*'*80)
+
+print(remove_suffix('bolero', 'ro'))
